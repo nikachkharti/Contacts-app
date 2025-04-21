8a5b29681433d4ccc3dc75f39fe8313351bffc72
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-04-21 21:46:17 +0400

    Mongo contracts and implementation

---
+++ Contacts/Contacts.Infrastructure/Configurations/MongoDbSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace Contacts.Infrastructure.Configurations
+{
+    public class MongoDbSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
