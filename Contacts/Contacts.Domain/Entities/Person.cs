9933afd6196192e53a3446eedc7776f534e927b1
ჩხარტიშვილი ნიკოლოზი <chkhartishvili.n@itstep.ge>
2025-04-21 21:25:32 +0400

    Entities

---
+++ Contacts/Contacts.Domain/Entities/Person.cs
@@ -0,0 +1,14 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Contacts.Domain.Entities
+{
+    public class Person
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public string Id { get; set; }
+        public string FullName { get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
